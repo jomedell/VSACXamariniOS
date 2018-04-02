91be25a3e9da9b10868874802b86882ffca1a183
Jorge Medellin <jorge.medellin@microsoft.com>
2018-04-02 15:40:29 -0500

    feat(Configure App): Create App
    #1 comment:
    
    • Create App

--- VSACXamariniOS_GitHub/AppDelegate.cs
+++ VSACXamariniOS_GitHub/AppDelegate.cs
@@ -5,6 +5,7 @@ namespace VSACXamariniOS_GitHub
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    //
     [Register("AppDelegate")]
     public class AppDelegate : UIApplicationDelegate
     {
